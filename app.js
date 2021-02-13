21b225feabbc19845c5db51aff60fdc70c837326
Jimmy Sawczuk <me@jimmysawczuk.com>
2021-02-13 10:49:28 -0500

    Inline SVGs

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 import "./style.less"
-
-import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import { faCameraRetro } from "@fortawesome/pro-duotone-svg-icons"
-
-library.add(faCameraRetro)
-
-export function init() {
-  dom.i2svg()
-}
